8957f2e6bf0dcf401baf4e5c34a54ece306b6ce1
killuaDev <you@example.com>
2022-12-25 11:44:03 +0000

    Cleaned up code and reformatted to better fit styleguide

--- matrix-algebra.rkt
+++ matrix-algebra.rkt
@@ -1,72 +1,49 @@
-; Matrix Algebra Suite in Scheme
-
-(define exampleVector1 '(1 2 3))
-(define exampleVector2 '(3 3 3))
-(define exampleScalar 3)
+#| Matrix Algebra Suite in Scheme |#
 
 (define (!= x y) (not (= x y)))
 
 (define (add-vectors v1 v2)
   (cond
-    ((not (= (length v1) (length v2))) (display "Vectors must be the same length to add"))
+    ((not (= (length v1) (length v2)))
+     	(display "Vectors must be the same length to add"))
     ((null? v1) '())
     (else
       (cons (+ (car v1) (car v2)) (add-vectors (cdr v1) (cdr v2))))))
 
 (define (subtract-vectors v1 v2)
   (cond
-    ((not (= (length v1) (length v2))) (display "Vectors must be the same length to subtract"))
+    ((not (= (length v1) (length v2)))
+     	(display "Vectors must be the same length to subtract"))
     ((null? v1) '())
     (else
       (cons (- (car v1) (car v2)) (subtract-vectors (cdr v1) (cdr v2))))))
 
-(define (scale v x)
-  (if (null? v)
-    '()
-    (cons (* x (car v)) (scale (cdr v) x))))
+(define (scale-vector v scalar)
+  (map (lambda (x) (* scalar x)) v))
 
-(define exampleMatrix1 '((1 2 3))) ; [[1 2 3]] : 1 * 3
-(define exampleMatrix2 '((1) (2) (3)))
-; [[1]
-;  [2]
-;  [3]] : 3 * 1
-(define exampleMatrix3 '((1 2 3) (4 5 6) (7 8 9)))
+(define (is-valid-matrix? m)
+  (apply = (map length m)))
 
+(define (get-dimensions-matrix-fast m)
+  (if (null? m) 
+    '() 
+    (list (length m) (length (car m)))))
 
-(define (is-valid-matrix? matrix)
+(define (get-dimensions-matrix-safe m)
   (cond
-    ((null? matrix) #t)
-    ((not (list? (car matrix))) #f)
-    (else (is-valid-matrix-tool matrix (length (car matrix))))
-    ))
+    ((not (is-valid-matrix? m))		(display "Invalid Matrix"))
+    ((null? m) 				'())
+    ((list (length m) (length (car m))))))
 
-(define (is-valid-matrix-tool matrix width)
-  (cond
-    ((null? matrix) #t)
-    ((not (list? (car matrix))) #f)
-    ((!= (length (car matrix)) width) #f)
-    (else (is-valid-matrix-tool (cdr matrix) (length (car matrix))))))
-
-(define (get-dimensions matrix)
-  (if (null? matrix) '() (list (length matrix) (length (car matrix)))))
-;; fast
-
-(define (get-dimensions-safe matrix)
-  (if (is-valid-matrix? matrix)
-    (if (null? matrix) '() (list (length matrix) (length (car matrix))))
-    (display "Invalid Matrix")
-    )
-  )
-
-(define (same-dimensions? m1 m2) 
-  (equal? (get-dimensions-safe m1) (get-dimensions-safe m2)))
+(define (same-dimensions-matrices? m1 m2) 
+  (equal? (get-dimensions-matrix-safe m1) (get-dimensions-matrix-safe m2)))
 
 (define (add-matrices m1 m2)
   (define (recurse m1 m2)
     (if (null? m1)
       '()
       (cons (add-vectors (car m1) (car m2)) (recurse (cdr m1) (cdr m2)))))
-  (if (same-dimensions? m1 m2)
+  (if (same-dimensions-matrices? m1 m2)
     (recurse m1 m2)
     (display "Matrices must be the same dimensions to add")))
 
@@ -75,56 +52,46 @@
     (if (null? m1)
       '()
       (cons (subtract-vectors (car m1) (car m2)) (recurse (cdr m1) (cdr m2)))))
-  (if (same-dimensions? m1 m2)
+  (if (same-dimensions-matrices? m1 m2)
     (recurse m1 m2)
     (display "Matrices must be the same dimensions to add")))
 
-(define (dot-product v1 v2)
+(define (vector*vector v1 v2)
   (apply + (map * v1 v2)))
 
-;; (dot-product-mv '((1 2) (3 4)) '(5 6)) -> '(17 39)
-;;[[1 2]   [[5     [[17
-;; [3 4] .   6]] =   39]]
-
-(define (dot-product-mv m v) 
+(define (matrix*vector m v) 
   (if (null? m) 
     '()
-    (cons (dot-product (car m) v) (dot-product-mv (cdr m) v))))
+    (cons (vector*vector (car m) v) (matrix*vector (cdr m) v))))
 
-(define (dot-product-mm m1 m2) ;; Works! Is there a better way?
-  (define (dot-product-mm-tool m1 m2)
-    (cond
-      ((null? m2) '())
-      (else
-	(cons (dot-product-mv m1 (car m2)) (dot-product-mm-tool m1 (cdr m2))))))
-  (transpose (dot-product-mm-tool m1 (transpose m2))))
-
-;; New, clean, transpose
+(define (matrix*matrix m1 m2)
+  (let ((columns (transpose m2)))
+    (map (lambda (row) (matrix*vector columns row)) m1)))
 (define (transpose M)
   (apply map list M))
 
-;; Tests
-(define expected '( (8 12 14) (10 20 30) (9880 9880 9880) (1 2 3) (9 15 15 5) (0 -1 1 -2) (1 2 3) (-1 -2 -3) (6 -3) #t #t #t #f #f (4 3) () ((58 64) (139 154))))
+#| Tests |#
+(define expected '( (8 12 14) (10 20 30) (9880 9880 9880) (1 2 3) (9 15 15 5) (0 -1 1 -2) (1 2 3) (-1 -2 -3) (6 -3) #t #t #f (4 3) () ((58 64) (139 154))))
 (define results (list
-  (scale '(8 12 14) 1) ; '(8 12 14)
-  (scale '(1 2 3) 10) ;  '(10 20 30)
-  (scale '(8 8 8) 1235) ; '(9880 9880 9880)
-  (add-vectors '(1 2 3) '(0 0 0)) ; '(1 2 3)
-  (add-vectors '(8 14 13 2) '(1 1 2 3)) ; '(9 15 15 5)
-  (add-vectors '(0 0 3 1) '(0 -1 -2 -3)) ; '(0 -1 1 -2)
-  (subtract-vectors '(1 2 3) '(0 0 0)) ; '(1 2 3)
-  (subtract-vectors '(0 0 0) '(1 2 3)) ; '(-1 -2 -3)
-  (subtract-vectors '(8 4) '(2 7)) ; '(6 -3)
-  (is-valid-matrix? '((1 2 3) (4 5 6))) ; #t
-  (is-valid-matrix? '((1 2 3))) ; #t
-  (is-valid-matrix? '()) ; #t
-  (is-valid-matrix? '(1 2 3)) ; #f
-  (is-valid-matrix? '((1 2 3) (1 2))) ; #f
-  (get-dimensions '((1 2 3) (4 5 6) (7 8 9) (10 11 12))) ; '(4 3)
-  (get-dimensions '()) ; '(0 0)
-  (dot-product-mm '((1 2 3) (4 5 6)) '((7 8) (9 10) (11 12)) ) ; '((58 64) (139 154))
+  (scale-vector '(8 12 14) 1) 
+  (scale-vector '(1 2 3) 10) 
+  (scale-vector '(8 8 8) 1235)
+  (add-vectors '(1 2 3) '(0 0 0))
+  (add-vectors '(8 14 13 2) '(1 1 2 3))
+  (add-vectors '(0 0 3 1) '(0 -1 -2 -3))
+  (subtract-vectors '(1 2 3) '(0 0 0))
+  (subtract-vectors '(0 0 0) '(1 2 3))
+  (subtract-vectors '(8 4) '(2 7))
+  (is-valid-matrix? '((1 2 3) (4 5 6)))
+  (is-valid-matrix? '((1 2 3)))
+  (is-valid-matrix? '((1 2 3) (1 2)))
+  (get-dimensions-matrix-fast '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
+  (get-dimensions-matrix-fast '())
+  (matrix*matrix '((1 2 3) (4 5 6)) '((7 8) (9 10) (11 12)) )
   ))
 
-(cond
-  ((equal? expected results) (newline) (display "All tests passed.") (newline) )
-  (else (newline) (display "Some tests failed.") (newline) (display results) ))
+(if (equal? expected results)
+  (begin (newline) (display "All tests passed.") (newline))
+  (begin (newline) (display "Some tests failed.")
+	 (newline) (display results)
+	 (newline) (display expected)))
